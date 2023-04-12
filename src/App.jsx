e8fe7a6c4e4fe341d55beb7ca0f59c9fdccf2498
Silvia <silviaratero@gmail.com>
2023-04-12 15:27:01 -0300

    Modificacion Descripicon

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { CartItem } from "./components/Cart/CartItem/CartItem";
 import Signup from "./components/Auth/Signup";
 import ProtectedRoutes from "./components/helpers/ProtectedRoutes";
 import { Link } from "react-router-dom";
+import Descipcion from "./components/Descripcion/descripcion";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
      
      <Nav />
      <Auth/>
+     <Descipcion />
      
     </div>
   );
