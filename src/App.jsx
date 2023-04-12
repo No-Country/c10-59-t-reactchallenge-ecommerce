8be5a30d4868dc65331bebd42e6965a8156eb156
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-04-12 10:25:28 -0300

    Add ListContainer

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 import Nav from "./components/NavBar/Nav";
 import Auth from "./components/Auth/Auth";
 import SingIn from "./components/Auth/SingIn";
+import { ListContainer } from "./components/ListContainer/ListContainer";
 
 
 function App() {
   return (
     <div className="App">
-
+      <ListContainer/>
       {/* <Nav/> */}
-     <SingIn/>
+     {/* <SingIn/> */}
     </div>
   );
 }
