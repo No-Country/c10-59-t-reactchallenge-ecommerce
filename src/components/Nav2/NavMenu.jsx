bad3f76c5e87d7acfe8912b6df1fd87d0e9277a0
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-04-20 15:34:15 -0300

    Fix title Auth

--- src/components/Nav2/NavMenu.jsx
+++ src/components/Nav2/NavMenu.jsx
@@ -1,27 +1,25 @@
 import { signOut } from "firebase/auth";
 import { Link } from "react-router-dom";
 import { auth } from "../../utils/firebase";
-import { useDispatch } from 'react-redux'
-import { setUserAuth } from '../../store/slices/userAuth.slice'
+import { useDispatch } from "react-redux";
+import { setUserAuth } from "../../store/slices/userAuth.slice";
 
 const NavMenu = ({ isopen }) => {
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const HandleSingOut = () => {
-    dispatch(setUserAuth(null))
-    signOut(auth)
-  }
+    dispatch(setUserAuth(null));
+    signOut(auth);
+  };
 
   return (
     <>
-    
-  <div className={`nav__items ${isopen && "open"}`}>
-  <Link to="/tienda">Tienda</Link>
-  <Link to="/pedidos">Pedidos</Link>
-  <Link to="/auth">Identificarse</Link>
-  <Link>Quienes Somos?</Link>
-  <button onClick={HandleSingOut}>Cerrar Sesion</button>
-  </div>
+      <div className={`nav__items ${isopen && "open"}`}>
+        <Link to="/tienda">Tienda</Link>
+        <Link to="/pedidos">Pedidos</Link>
+        <Link to="/auth">Identificarse</Link>
+        <Link>¿Quienes Somos?</Link>
+        <button onClick={HandleSingOut}>Cerrar Sesion</button>
+      </div>
     </>
   );
 };
